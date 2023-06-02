0825544c85e8f4b1965ac0a49000941e349b9f7e
Jose Angel Vazquez Torres <joseangelvazqueztorres@outlook.com>
2023-06-02 12:27:15 -0600

    14 Implementando onChanges en formulario reactivo

--- inventariofrontend/src/app/components/login-form/login-form.component.ts
+++ inventariofrontend/src/app/components/login-form/login-form.component.ts
@@ -1,28 +1,39 @@
-import { Component,Input } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Component,Input, OnChanges, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent {
-@Input() isSignUp: boolean= true;
+export class LoginFormComponent implements OnChanges{
+@Input() isSignUp!: boolean;
 
 formUser!: FormGroup;
+defaultFields = {
+  email: new FormControl('',[Validators.required,Validators.email]),
+  password: new FormControl('',Validators.required)
+}
+extraFields = {
+name: new FormControl(''),
+lastname: new FormControl('')
+}
 constructor(
   private fb:FormBuilder
 ){
+}
+ngOnChanges(changes: SimpleChanges): void {
+  console.log(changes);
   this.initForm();
 }
-
 initForm(){
-  this.formUser = this.fb.group({
-    name:[''],
-    lastname:[''],
-    email:['', Validators.required],
-    password:['']
-  })
+  let userFields = {...this.defaultFields};
+  if(this.isSignUp){
+    userFields = {...this.defaultFields, ...this.extraFields};
+  }
+  this.formUser = this.fb.group(
+    userFields
+  )
 }
 
 onSubmit(){
