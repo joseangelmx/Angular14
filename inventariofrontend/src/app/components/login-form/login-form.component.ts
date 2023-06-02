8c106e081c189f06df5da5e052e834a3bcaa6cbd
Jose Angel Vazquez Torres <joseangelvazqueztorres@outlook.com>
2023-06-01 20:33:47 -0600

    11 Configurando el comportamiento del form en base a inputs

--- inventariofrontend/src/app/components/login-form/login-form.component.ts
+++ inventariofrontend/src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent {
-
+@Input() isSignUp: boolean= true;
 }
