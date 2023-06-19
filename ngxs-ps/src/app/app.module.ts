e80a1c756a9811de05caac5b0076aaadb0db44c2
Jose Angel Vazquez Torres <joseangelvazqueztorres@outlook.com>
2023-06-19 13:51:15 -0600

    33 ngxs parte 1

---
+++ ngxs-ps/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
